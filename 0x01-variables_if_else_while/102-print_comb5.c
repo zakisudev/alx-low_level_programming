30ef6545e84afc87c462ba73b583746fdbe8a990
zakisu250 <zakisu250@gmail.com>
2023-02-17 10:22:57 -0800

    Display two double digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all single digit numbers of base 10
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 98; x++)
+	{
+	for (y = x + 1; y <= 99; y++)
+	{
+	putchar((x/10) + '0');
+	putchar((x % 10) + '0');
+	putchar(' ');
+	putchar((y / 10) + '0');
+	putchar((y % 10) + '0');
+	if (x ==98 && y == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
