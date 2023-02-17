a00d28801d1e5259bd6dbe612f7648111138c587
zakisu250 <zakisu250@gmail.com>
2023-02-17 07:25:26 -0800

    Display base 10 numberswith comma separation

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num =0; num < 10; num++)
+	{
+		if (num == 9)
+		{
+		putchar(57);
+		}
+		else
+		{
+		putchar(48 + num);
+		putchar(',');
+		putchar(' ');	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
