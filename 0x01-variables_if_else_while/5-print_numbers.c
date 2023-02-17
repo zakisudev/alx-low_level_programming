a6dc1a180d2070119e987cd51b2f339b26c5389b
zakisu250 <zakisu250@gmail.com>
2023-02-17 06:38:08 -0800

    Display base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Displays base 10 numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
