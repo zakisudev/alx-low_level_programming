aea4974941ae995a444368ec1230fe4260c376c4
zakisu250 <zakisu250@gmail.com>
2023-02-17 06:12:50 -0800

    Display lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints lowercase alphabets
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	for (c == 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
