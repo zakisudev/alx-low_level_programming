82baa3726132a9be37be2cbe28887efc0ebd46c5
zakisu250 <zakisu250@gmail.com>
2023-02-21 10:36:19 -0800

    Print time table of n value

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,19 +24,19 @@ void print_times_table(int n)
 				_putchar(' ');
 				prod = num * multi;
 				if (prod <= 99)
-				_putchar(' ');
+					_putchar(' ');
 				if (prod <= 9)
-				_putchar(' ');
+					_putchar(' ');
 				if (prod >= 100)
-					{
-						_putchar((prod / 100) + '0');
-						_putchar((prod / 10) % 10 + '0');
-					}
-					else if (prod <= 99 && prod >= 10)
-					{
-						_putchar((prod / 10) + '0');
-					}
-				_putchar((prod % 10) + '0');
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+			_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
