9cf0cd3797df2d59ea858a8ba0e4cad2db87359f
zakisu250 <zakisu250@gmail.com>
2023-02-21 10:43:12 -0800

    Multiples of 3 or 5 upto 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print sum of all multiples of 3 or 5 upto 1024
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n, x = 0;
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0)
+		{
+			x += n;
+		}
+		n++;
+	}
+	printf("%d\n", x);
+	return (0);
+}
