c558881f3ec9fa1872a1db09d01b0004d6a0f223
zakisu250 <zakisu250@gmail.com>
2023-02-21 07:24:01 -0800

    Display natural numbers starting from n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - Print natural numbers to 98
  *
- * n: number to start from
+ *@n: number to start from
  *
  * Return: Always 0
  */
@@ -13,7 +13,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n<= 98; n++)
+		for (; n <= 98; n++)
 		{
 			if (n == 98)
 			{
