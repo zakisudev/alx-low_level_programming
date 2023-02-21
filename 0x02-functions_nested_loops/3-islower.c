4e13ac14dbc892726bdf21dcbea876c337c4be6d
zakisu250 <zakisu250@gmail.com>
2023-02-21 04:36:56 -0800

    Check for lowercase characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 /**
- * _islower - check lowercase
+ * _islower - check c if lowercase
  *
  * Return: Always 0
  */
@@ -11,7 +11,7 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-	return(1);
+	return (1);
 	}
 	return (0);
 }
