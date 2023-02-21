4d8754bbff067535c647e39d90bb4ff545fc1c94
zakisu250 <zakisu250@gmail.com>
2023-02-21 10:54:27 -0800

    Print 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Print the first 52 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x = 0;
+	long y = 1, z = 2;
+
+	while (x < 50)
+	{
+		if (x == 0)
+		printf("%d", y);
+		else if (x == 1)
+		printf(", %ld", z);
+		else
+		{
+			z += y;
+			y = z - y
+			printf(", %ld", z);
+		}
+		++x;
+	}
+	printf("\n");
+	return (0);
+}
