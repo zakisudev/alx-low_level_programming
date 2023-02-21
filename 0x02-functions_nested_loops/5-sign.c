9ae7299cdcbd01ca90c0dec26fbc9e79966eda7b
zakisu250 <zakisu250@gmail.com>
2023-02-21 04:51:26 -0800

    Print the size of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _print_sign - print sign of a number
+ *
+ * Return: 1 if n is positive 0 if n is zero -1 if n is negative
+ */
+
+int _isalpha(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
