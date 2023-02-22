b92aff5926c9f84ce340fd7aa6028c34bb4695bd
zakisu250 <zakisu250@gmail.com>
2023-02-22 07:27:52 -0800

    Display the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,20 @@
 
 #include <stdio.h>
 
-int main() 
+int main(void)
 {
-    int i;
-        unsigned long first = 1, second = 2, next;
+	int i;
+	unsigned long first = 1, second = 2, next;
 
-	    printf("%lu, %lu, ", first, second);
+	printf("%lu, %lu, ", first, second);
 
-	        for (i = 2; i < 98; i++) {
-		        next = first + second;
-			        printf("%lu, ", next);
-				        first = second;
-					        second = next;
-						    }
-
-						        printf("\n");
-		   return 0;
+	for (i = 2; i < 98; i++)
+	{
+		next = first + second;
+		printf("%lu, ", next);
+		first = second;
+		second = next;
+	}
+	printf("\n");
+	return (0);
 }
