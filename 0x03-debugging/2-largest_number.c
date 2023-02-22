9f879eb1c13674b57b0e181cd240dd1ad7fce833
zakisu250 <zakisu250@gmail.com>
2023-02-22 06:27:56 -0800

    Largest of three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,12 +20,9 @@ int largest_number(int a, int b, int c)
 			        {
 					        largest = b;
 						    }
-		        else if (c > b)
+		        else if (c > a && c > b)
 				    {
 					            largest = c;
-						        }
-		else 
-			largest = c;
-
+			}
 			    return (largest);
 }
