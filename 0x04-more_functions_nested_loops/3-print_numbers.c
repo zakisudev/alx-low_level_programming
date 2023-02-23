ea88412145d409c98db4ad5e6b969b510db4aa77
zakisu250 <zakisu250@gmail.com>
2023-02-23 01:05:52 -0800

    Print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Print 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
