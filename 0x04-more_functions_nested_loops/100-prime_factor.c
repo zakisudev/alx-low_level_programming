8cfeb072977a6404ea125022af215f2e61d3686b
zakisu250 <zakisu250@gmail.com>
2023-02-23 13:39:31 -0800

    Print prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the largest prime factor of the number 612852475143
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long x, maxf;
+	long number = 612852475143;
+	double square = srqt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%d\n", maxf);
+	return (0);
+}
