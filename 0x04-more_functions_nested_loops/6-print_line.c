74a36c3651d4a095f81e0d4425f2d96ae803b195
zakisu250 <zakisu250@gmail.com>
2023-02-23 04:36:42 -0800

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - Print line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		if (n < 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
