10bd8b222199239bb02d70c52dcbc08d480e18a3
zakisu250 <zakisu250@gmail.com>
2023-02-23 12:16:28 -0800

    Print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_square - Prints square #
+ * print_square - Prints a square n times
  * @size: number of squares
  * Return: empty
  */
@@ -15,14 +15,14 @@ void print_square(int size)
 		putchar('\n');
 	}
 	else
-		{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar(35);
-			}
-			putchar('\n');
-		}
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
