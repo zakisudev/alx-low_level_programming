2614370e3d516227e85935ce09d52bd23da991cc
zakisu250 <zakisu250@gmail.com>
2023-02-27 10:21:51 -0800

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ *
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
