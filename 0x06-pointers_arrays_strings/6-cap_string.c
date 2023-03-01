b9ea3469e8be530f1507f6e5801d70af36b1743d
zakisu250 <zakisu250@gmail.com>
2023-03-01 10:38:44 -0800

    Change all strings to cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,19 +2,25 @@
 
 /**
  * cap_string - Capitalizes all words of a string
- * @src: THe string to be capitalized
+ * @str: THe string to be capitalized
  * Return: pointer to the changed string
  */
 
 char *cap_string(char *str)
 {
 	int index = 0;
-	
+
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 		index++;
-		if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '{' || str[index - 1] == '}' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == 0)
+		if (str[index - 1] == ' ' || str[index - 1] == '\t' ||
+			str[index - 1] == '\n' || str[index - 1] == ',' ||
+			str[index - 1] == ';' || str[index - 1] == '.' ||
+			str[index - 1] == '!' || str[index - 1] == '?' ||
+			str[index - 1] == '"' || str[index - 1] == '{' ||
+			str[index - 1] == '}' || str[index - 1] == '(' ||
+			str[index - 1] == ')' || str[index - 1] == 0)
 		str[index] -= 32;
 		index++;
 	}
