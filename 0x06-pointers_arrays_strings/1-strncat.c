07064c1140407ad8b1b4a0523d1cde7cdf4d65bc
zakisu250 <zakisu250@gmail.com>
2023-03-01 09:46:31 -0800

    Concatenate two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,13 @@
  * Return: Pointer to the resulting string dest
  */
 
-char char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
-	
-	while(dest[index++])
+
+	while (dest[index++])
 	dest_len++;
-	for(index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 	return (dest);
 }
