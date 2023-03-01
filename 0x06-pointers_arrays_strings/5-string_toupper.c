cf26d29f8a691cf1db2682de51e43a58dd15105e
zakisu250 <zakisu250@gmail.com>
2023-03-01 10:29:42 -0800

    Change string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,12 +9,12 @@
 char *string_toupper(char *str)
 {
 	int index = 0;
-	
+
 	while (str[index])
 	{
-	       if (str[index] >= 'a' && str[index] <= 'z')
-		       str[index] -= 32;
-	       index++;
+	if (str[index] >= 'a' && str[index] <= 'z')
+		str[index] -= 32;
+		index++;
 	}
 	return (str);
 }
