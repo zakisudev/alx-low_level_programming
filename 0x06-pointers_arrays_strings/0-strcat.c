95255c41a7da986502de9e5651cf15804186408c
zakisu250 <zakisu250@gmail.com>
2023-03-01 00:25:26 -0800

    Copy string to destination with pointer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: destination of the string
+ * @src: source of the string
+ * Return: Pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dLen = 0, i;
+
+	while(dest[dLen])
+	{
+		dLen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dLen] = src[i];
+		dLen++;
+	}
+	dest[dLen] = '\0';
+	return (dest);
+}
