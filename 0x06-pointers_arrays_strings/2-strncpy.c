f80f2416b3dbe00751d3b76777944547b5c2cfde
zakisu250 <zakisu250@gmail.com>
2023-03-01 00:42:00 -0800

    Copy string of n bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy a string an inputted number of bytes
+ * @dest: store for string copy
+ * @src: source of the string
+ * Return: Pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+	
+	while(src[index++])
+	src_len++;
+	for(index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = src_len; index <  n; index++)
+		dest[index] ='\0';
+	return (dest);
+}
