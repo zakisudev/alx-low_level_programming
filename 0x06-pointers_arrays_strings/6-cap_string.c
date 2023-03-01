8c6ff88c5122939eec00f5512da45a49867d1a9a
zakisu250 <zakisu250@gmail.com>
2023-03-01 10:54:10 -0800

    Capitalize all string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,32 +4,29 @@
  * cap_string - Capitalizes all words of a string
  * @str: THe string to be capitalized
  * Return: pointer to the changed string
- */
+*/
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int index = 0;
+	int count;
 
-	while (str[index] != '\0')
+	count = 0;
+	while (s[count] != '\0')
+	if (s[0] >= 97 && s[0] <= 122)
 	{
-		if (str[index] >= 97 && str[index] <= 122)
-		{
-			str[0] = str[0] - 32;
-		}
-		if (str[index] == ' ' || str[index] == '\t' ||
-			str[index] == '\n' || str[index] == ',' ||
-			str[index] == ';' || str[index] == '.' ||
-			str[index] == '!' || str[index] == '?' ||
-			str[index] == '"' || str[index] == '{' ||
-			str[index] == '}' || str[index] == '(' ||
-			str[index] == ')' || str[index] == 0)
-		{
-			if (str[index + 1] >= 97 && str[index + 1] <= 122)
-			{
-				str[index + 1] = str[index + 1] - 32;
-			}
-		}
-		index++;
+		s[0] = s[0] - 32;
 	}
-	return (str);
+	if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n' 
+			|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+			|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+			|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+			|| s[count] == '{' || s[count] == '}')
+	{
+		if (s[count + 1] >= 97 && s[count + 1] <= 122)
+		{
+			s[count + 1] = s[count + 1] - 32;
+		}
+		count++;
+	}
+	return (s);
 }
