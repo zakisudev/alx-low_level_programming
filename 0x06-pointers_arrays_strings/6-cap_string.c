6b68373160006737949c149d6c8146b3034a4f01
zakisu250 <zakisu250@gmail.com>
2023-03-01 21:06:51 -0800

    Capitalize all strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,27 +6,27 @@
  * Return: pointer to the changed string
 */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 	int count;
 
 	count = 0;
 	while (s[count] != '\0')
-	if (s[0] >= 97 && s[0] <= 122)
+	if (str[0] >= 97 && str[0] <= 122)
 	{
-		s[0] = s[0] - 32;
+		str[0] = s[0] - 32;
 	}
-	if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n' 
-			|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-			|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-			|| s[count] == '"' || s[count] == '(' || s[count] == ')'
-			|| s[count] == '{' || s[count] == '}')
+	if (str[count] == ' ' || str[count] == '\t' || str[count] == '\n'
+			|| str[count] == ',' || str[count] == ';' || str[count] == '.'
+			|| str[count] == '.' || str[count] == '!' || str[count] == '?'
+			|| str[count] == '"' || str[count] == '(' || str[count] == ')'
+			|| str[count] == '{' || str[count] == '}')
 	{
-		if (s[count + 1] >= 97 && s[count + 1] <= 122)
+		if (str[count + 1] >= 97 && str[count + 1] <= 122)
 		{
-			s[count + 1] = s[count + 1] - 32;
+			str[count + 1] = str[count + 1] - 32;
 		}
 		count++;
 	}
-	return (s);
+	return (str);
 }
