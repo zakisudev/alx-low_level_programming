1362849dc5646f5b3308879e92992a7372096df1
zakisu250 <zakisu250@gmail.com>
2023-03-02 04:14:00 -0800

    Print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,57 +1,61 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * print_buffer - Prints a buffer 10 bytes at a time,
- * starting with byte position, then showing the hex
- * content then displaying printable characters
- * @b: the buffer to be printed
- * @size: the number of bytes to be printed from the buffer
- * Return: void
- */
-
+ *  * print_buffer - prints a buffer
+ *   * Description:
+ *    *	first column is position of the first byte (in hexadecimals)
+ *     *		of that line
+ *      *	each line represents 10 bytes from the buffer
+ *       *	the next 5 columns is the content in hexadecimals, 2 bytes at a time,
+ *        *		separated by a space
+ *         *	then the content of the buffer is printed, if it's not a printable
+ *          *		character then print.
+ *           * @b: the buffer that will be printed
+ *            * @size: the number of bytes to be printed from buffer
+ *             *
+ *              * Return: void - nothing
+ *               */
 void print_buffer(char *b, int size)
 {
-	int byte, index;
+		int line_num, lines_to_be_printed, byte_num, remainder = (10 - size % 10);
 
-	if (size <= 0)
-		printf("\n");
-	
-	for (byte = 0; byte < size; byte += 10)
-	{
-		printf("%08x: ", byte);
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-			{
-				printf(" ");
-			}
-			else if ((index % 2) != 0 && index != 0)
-			{
-				printf(" ");
-			}
-			else
-			{
-			printf("%02x", *(b + index + byte));
-
-			}
-		}
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
-
-			else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-
-			else
-				printf(",");
-		}
-
-		if (byte >= size)
-			continue;
-
-		printf("\n");
-	}
+			if (size % 10 != 0)
+						lines_to_be_printed = size / 10 + 1;
+				else
+							lines_to_be_printed = size / 10;
+					for (line_num = 0; size > 0; line_num += 10)
+							{
+										printf("%08x: ", line_num);
+												for (byte_num = line_num; byte_num < line_num + 10 &&
+																		byte_num < size; byte_num++)
+															{
+																			if (isprint(b[byte_num]) == 0)
+																								printf("%02x", b[byte_num]);
+																						else
+																											printf("%02x", b[byte_num]);
+																									if (byte_num % 2 != 0)
+																														printf(" ");
+																											}
+														if ((line_num / 10 == lines_to_be_printed - 1) &&
+																				remainder != 0 && remainder != 10)
+																		for (; remainder > 0; remainder--)
+																						{
+																											printf("  ");
+																															if (remainder % 2 != 0)
+																																					printf(" ");
+																																		}
+																for (byte_num = line_num; byte_num < line_num + 10 &&
+																						byte_num < size; byte_num++)
+																				if (isprint(b[byte_num]) == 0)
+																									printf(".");
+																			else
+																								printf("%c", b[byte_num]);
+																		printf("\n");
+																				if (byte_num >= size)
+																								break;
+																					}
+						if (size <= 0)
+									printf("\n");
 }
