4599cb3494abe28b3f89d0c0d5dd916b161bb369
zakisu250 <zakisu250@gmail.com>
2023-03-02 00:05:23 -0800

    Capitalize all strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,32 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string
- * @str: source string
- * Return: String capitalized
+ * cap_string - Function that capitalizes all words of a string
+ * @s: s is the world to be analyzed
+ * Return: Always 0.
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int con, index, count;
-	char chars[] = {' ', ',', ';', '.', '!', '?', '"',
-		'(', ')', '{', '}',  '\t', '\n', '\0'};
+	int i = 0;
 
-	con = 32;
-	for (index = 0; str[index] != '\0'; index++)
+	while (s[i] != '\0')
 	{
-		if (str[index] >= 'index' && str[index] <= 'z')
+		if (s[0] <= 122 && s[0] >= 97)
 		{
-			str[index] =  str[index] - con;
+			s[0] = s[0] - 32;
 		}
-		con = 0;
-		for (count = 0; chars[count] != '\0'; count++)
+		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
+		s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
+		s[i] == '!' || s[i] == '?' || s[i] == '(' ||
+		s[i] == ')' || s[i] == '{' || s[i] == '}')
 		{
-			if (chars[count] == entry[index])
+		if (s[i + 1] <= 122 && s[i + 1] >= 97)
 			{
-				conversion = 32;
-				break;
+			s[i + 1] = s[i + 1] - 32;
 			}
 		}
+	i++;
 	}
-	return (entry);
+	return (s);
 }
