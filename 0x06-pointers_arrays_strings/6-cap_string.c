ad57605e5a811445a83646425afbba32e0e75885
zakisu250 <zakisu250@gmail.com>
2023-03-01 21:09:52 -0800

    Capitalize all words of a strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,10 +11,10 @@ char *cap_string(char *str)
 	int count;
 
 	count = 0;
-	while (s[count] != '\0')
+	while (str[count] != '\0')
 	if (str[0] >= 97 && str[0] <= 122)
 	{
-		str[0] = s[0] - 32;
+		str[0] = str[0] - 32;
 	}
 	if (str[count] == ' ' || str[count] == '\t' || str[count] == '\n'
 			|| str[count] == ',' || str[count] == ';' || str[count] == '.'
