e5f720e1dce70038ebe76fc72636cc4e7824982b
zakisu250 <zakisu250@gmail.com>
2023-03-02 10:43:13 -0800

    Print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -21,11 +21,11 @@ void print_buffer(char *b, int size)
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
+		printf("%08X: ", o);
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
-				printf("%02x", *(b + o + i));
+				printf("%02X", *(b + o + i));
 			else
 				printf("  ");
 			if (i % 2)
