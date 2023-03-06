9de47880baf663f37e88b0b5427ba23b440baaee
zakisu250 <zakisu250@gmail.com>
2023-03-06 03:07:00 -0800

    First occurrence of byte in s

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - search for a string in a set of bytes
+ * @s: the string to be checked
+ * @accept: the string to be matched
+ * Return: the pointer to the bye in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	unsigned int s_index, accept_index;
+	char *ch;
+
+	for (s_index = 0; s[s_index] != '\0'; s_index++)
+	{
+		if (s[s_index] == accept[accept_index])
+			ch = &accept[accept_index];
+			return (ch);
+	}
+	return (NULL);
+}
