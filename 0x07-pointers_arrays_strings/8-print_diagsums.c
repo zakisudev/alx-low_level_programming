b58b27d3c86af7e03902d7639e341916a46ce825
zakisu250 <zakisu250@gmail.com>
2023-03-06 04:08:33 -0800

    Diagonal sums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-int _strlen(char *s);
 /**
  * print_diagsums - Prints sum of a diagonal
  * @a: square matrix of diagonals
@@ -13,11 +12,9 @@ void print_diagsums(int *a, int size)
 	int sum_mat = 0, sum_tam = 0, index;
 
 	for (index = 0; index <= size * size - 1; index += size + 1)
-	{
 		sum_mat += a[index];
-	}
 	printf("%d, ", sum_mat);
-	for (index = (size * size - 1) - (size - 1); index <= size - 1;
+	for (index = (size * size - 1) - (size - 1); index >= size - 1;
 	index -= size - 1)
 	{
 		sum_tam += a[index];
