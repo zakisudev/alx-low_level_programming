c309beb9f5cf6e7c048e7f74516021536570409e
zakisu250 <zakisu250@gmail.com>
2023-03-06 02:51:41 -0800

    Match accept string in s

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn - Get a length of substring
+ * @s: the strig to be checked
+ * @accept: the string to be matched
+ * Return: the length of the substring that is a match
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+	unsigned int s_index, accept_index, count = 0, count_before;
+
+	for (s_index = 0; s[s_index] != '\0'; s_index++)
+	{
+		count_before = count;
+		for (accept_index = 0; accept[accept_index] != '\0'; accpet_index++)
+		{
+		if (s[s_index] == accept[accept_index])
+			count++;
+		}
+		if (count_before == count)
+			return (count);
+	}
+	return (count);
+}
