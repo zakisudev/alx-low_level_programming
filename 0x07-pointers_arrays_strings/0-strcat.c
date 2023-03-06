e1ad841cad4e63a33631b7e07d162c9ff780d8b3
zakisu250 <zakisu250@gmail.com>
2023-03-06 01:40:24 -0800

    Fill memory with constant byte b

--- 0x07-pointers_arrays_strings/0-strcat.c
+++ 0x07-pointers_arrays_strings/0-strcat.c
@@ -3,15 +3,17 @@
 
 /**
  * _memset - Fills a memory with a constant byte, b
- * @index: values of the memory
- * Return: Pointer to the resulting string dest
+ * @s: memory that is going to be filled
+ * @n: amount of bytes to fill the memory
+ * @b: the constant to fill the memory
+ * Return: Pointer to the memory
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 
 	unsigned int index;
-	
+
 	for (index = 0; index < n; index++)
 	{
 		s[index] = b;
