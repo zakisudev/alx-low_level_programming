477bade0caae1eb269bd2e4f95a225029deebd0c
zakisu250 <zakisu250@gmail.com>
2023-03-06 03:50:43 -0800

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+int _strlen(char *s);
+/**
+ * print_chessboard - Prints a chessboard
+ * 
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int fc, sc;
+
+	for (fc = 0; fc < 8; fc++)
+	{
+		for (sc = 0; sc < 8; sc++)
+		{
+			_putchar(a[fc][sc]);
+		}
+		_putchar('\n);
+	}
+}
