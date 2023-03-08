6cce9f71e04c2bb92dcf440df575e738019f344d
zakisu250 <zakisu250@gmail.com>
2023-03-08 03:22:25 -0800

    String length in recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,6 +13,6 @@ int _strlen_recursion(char *s)
 
 	if (*s == '\0')
 		return (0);
-	len = 1 +  _strlen_recursion(s + 1);
-	return (len);
+	length = 1 +  _strlen_recursion(s + 1);
+	return (length);
 }
