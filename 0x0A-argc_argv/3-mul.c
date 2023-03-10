c19dcd6ea8d3c4da4a301b22615ff5419f1ba737
zakisu250 <zakisu250@gmail.com>
2023-03-10 10:03:28 -0800

    Multiply two arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 			printf("Error\n");
 			return (1);
 		}
-		prod = atoi(argv[1] * atoi(argv[2]));
+		prod = atoi(argv[1]) * atoi(argv[2]));
 		printf("%d\n", prod);
 	}
 	return (0);
