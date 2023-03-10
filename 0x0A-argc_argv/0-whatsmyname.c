9cadf3f33f38c8088624a666833a2bae8dfd84af
zakisu250 <zakisu250@gmail.com>
2023-03-10 13:24:51 -0800

    Print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,9 @@
  * Return: 0 (success)
  */
 
-int main(int argc __attribute__ ((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(voic) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
