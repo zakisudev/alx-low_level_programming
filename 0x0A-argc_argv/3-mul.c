01e00b4d174b7646ef1c8fcdff9dc33a77b805e4
zakisu250 <zakisu250@gmail.com>
2023-03-10 09:58:45 -0800

    Multiply two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints the of two arguments passed to it
+ * @argc: counts of arguements on command line
+ * @argv: pointer to the array of command line arguements
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, prod = 1;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (argc != 2)
+		{
+			printf("Error");
+		}
+		prod *= argv[i];
+		printf("%s\n", prod);
+	}
+	return (0);
+}
