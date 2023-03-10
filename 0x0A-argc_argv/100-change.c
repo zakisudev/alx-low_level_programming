bb1c6d1ac8db32ae9eee9f256ee29782da1505e5
zakisu250 <zakisu250@gmail.com>
2023-03-10 10:24:29 -0800

    Print minimum number of change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,34 +10,31 @@
  *       */
 int main(int argc, char *argv[])
 {
-		int count = 0, amount;
+	int count = 0, amount;
 
-			if (argc != 2)
-					{
-								printf("Error\n");
-										return (1);
-											}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	amount = atoi(argv[1]);
+	if (amount < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (; amount >= 25; amount -= 25)
+		count++;
+	for (; amount >= 10; amount -= 10)
+		count++;
+	for (; amount >= 5; amount -= 5)
+		count++;
+	for (; amount >= 2; amount -= 2)
+		count++;
+	for (; amount >= 1; amount -= 1)
+		count++;
 
-				amount = atoi(argv[1]);
+	printf("%d\n", count);
 
-					if (amount < 0)
-							{
-										printf("0\n");
-												return (0);
-													}
-
-						for (; amount >= 25; amount -= 25)
-									count++;
-							for (; amount >= 10; amount -= 10)
-										count++;
-								for (; amount >= 5; amount -= 5)
-											count++;
-									for (; amount >= 2; amount -= 2)
-												count++;
-										for (; amount >= 1; amount -= 1)
-													count++;
-
-											printf("%d\n", count);
-
-												return (0);
+	return (0);
 }
