569df61562888273f65c1de87bedd3bc8b720ede
zakisu250 <zakisu250@gmail.com>
2023-03-10 13:05:14 -0800

    Print argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: pointer to the array of command line arguments
  * Return: 0 (success)
  */
-int main(int argc __attribute__ ((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
