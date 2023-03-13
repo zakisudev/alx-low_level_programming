a0ab88e53b55201eb7195c8fca49a16cbfcd602d
zakisu250 <zakisu250@gmail.com>
2023-03-13 02:20:11 -0700

    Print two dimesional arrays

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - creates a memory space for a 2D array and intializes to 0
+ * @width: the width of the array
+ * @height: the height of the array
+ * 
+ * Return: pointer to the 2D array, or NULL if failure, or width or height <= 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr;
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+	
+	ptr = malloc(sizeof(int *) * height);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+		if (ptr[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
