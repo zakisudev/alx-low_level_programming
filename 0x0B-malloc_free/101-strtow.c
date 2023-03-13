a1efa0b96bc962c208c2fc9e97b8836c13b9d857
zakisu250 <zakisu250@gmail.com>
2023-03-13 05:10:21 -0700

    String to word

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,8 +7,7 @@ int *_strlen_array(char *str);
 int _strlen(char *str);
 
 /**
- * strtow - splits a string to an array of word, 
- * last element of array = NULL
+ * strtow - splits a string to an array of word, last element of array = NULL
  * @str: the string that will be split into words
  *
  * Return: pointer to the array of words
@@ -20,19 +19,24 @@ char **strtow(char *str)
 
 	if (str == NULL || _strlen(str) == 0 || count_words(str) == 0)
 		return (NULL);
-	num_of_words = count_words(str);
-	ptr2strlen = _strlen_array(str);
+	num_of_words = count_words(str); /* number of words in string from 1 */
+	ptr2strlen = _strlen_array(str); /* an array of the strlen of words */
+	/* elements of array count strlen from 0 */
 	if (ptr2strlen == NULL)
 		return (NULL);
+	/* memory allocation to pointer to strings (the rows) */
 	ptr2str = malloc(sizeof(char *) * (num_of_words + 1));
+	/* +1 to incl final pointer to NULL -> WRONG? too much memory*/
+
 	for (i = 0; i < num_of_words; i++)
 	{
 		ptr2str[i] = malloc(sizeof(char) * (ptr2strlen[i] + 2));
+/* +2 bc strlen from 0 & incl. null terminator -> WRONG? too much memory */
 		if (ptr2str[i] == NULL)
 		{
-			for (; i >= 0; i--)
+			for (; i >= 0; i--) /* free allocation to strings */
 				free(ptr2str[i]);
-			free(ptr2str);
+			free(ptr2str); /* free allocation to pointers */
 		}
 	}
 	for (i = 0; i < num_of_words; i++)
@@ -43,9 +47,10 @@ char **strtow(char *str)
 				;
 			ptr2str[i][j] = str[k];
 		}
-		ptr2str[i][j] = '\0';
+		ptr2str[i][j] = '\0'; /* adding null terminator to string */
 	}
-	ptr2str[i] = NULL;
+	ptr2str[i] = NULL; /* last element of array is NULL */
+
 	free(ptr2strlen);
 	return (ptr2str);
 }
@@ -65,20 +70,22 @@ int count_words(char *str)
 		for (; str[k] != '\0'; k++)
 		{
 			if (i == 0 && str[k] == ' ' && k == 0)
-			{
+			{ /* to take account of leading spaces in string */
 				while (str[k] == ' ')
 					k++;
 				if (str[k] == '\0')
 					return (0);
 			}
+
 			if (str[k] == ' ')
 			{
 				while (str[k] == ' ')
 					k++;
-				break;
+				break;	/* breaks after trailing spaces */
 			}
 		}
 	}
+
 	return (i);
 }
 
@@ -93,27 +100,31 @@ int *_strlen_array(char *str)
 	int i, k = 0, len, *ptr2strlen;
 
 	ptr2strlen = malloc(sizeof(int) * count_words(str) + 1);
+	/* count_words will return number starting from 0 */
 	if (ptr2strlen == NULL)
 		return (NULL);
+
 	for (i = 0; i <= count_words(str); i++)
 	{
 		len = 0;
 		for (; str[k] != '\0'; k++)
 		{
 			if (i == 0 && str[k] == ' ' && k == 0)
-			{
+			{ /* to take account of leading spaces in string */
 				while (str[k] == ' ')
 					k++;
 			}
+
 			if (str[k] == ' ')
 			{
 				while (str[k] == ' ')
 					k++;
-				break;
+				break;	/* breaks after trailing spaces */
 			}
 			ptr2strlen[i] = len++;
 		}
 	}
+
 	return (ptr2strlen);
 }
 
@@ -129,5 +140,6 @@ int _strlen(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
+
 	return (i);
 }
