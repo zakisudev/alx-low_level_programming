a19b1629540807d29fe66e46fbad06bca4cccad8
zakisu250 <zakisu250@gmail.com>
2023-03-13 01:32:28 -0700

    Create and array and assign c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array - create an array
+ * @c: character to assign to array
+ * @size: Size of the array
+ *
+ * Return: Pointer to array or NULL otherwise
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr = malloc(sizeof(c)*size);
+
+	if (size == 0)
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
