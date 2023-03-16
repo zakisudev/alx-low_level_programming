1dabcd29204c36eba3ba3dd3468c12e172924956
zakisu250 <zakisu250@gmail.com>
2023-03-16 02:24:09 -0700

    Concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,52 @@
-#include <stdlib.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
-unsigned int _strlen(char *str);
 /**
- * string_nconcat - cocatenate n byte of string from s2 to s1
- * @s1: the first string
- * @s2: the string to be concatenated
- * @n: number of bytes in string s2
- * Return: Pointer topointer to a newly allocated sace or NULL
- */
-
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int i, j;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-	if (n > _strlen(s2))
-		n = _strlen(s2);
-		ptr = malloc(sizeof(char) * (_strlen(s1) + n + 1));
-	if (ptr == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
 
-	for (i = 0, j = 0; j < _strlen(s1); i++, j++)
-		ptr[i] = s1[j];
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
 
-	for (j = 0; j < n; i++, j++)
-		ptr[i] = s2[j];
-		ptr[i] = '\0';
-	return (ptr);
-}
-
-/**
- * _strlen - calculates the length of a string
- * @str: the string used in the calculation
- *
- * Return: the length of the string
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
-
-	if (str == NULL)
-		return (0);
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
