583acf2617e13bf1ec6fa900ccb39049ee8ecd39
zakisu250 <zakisu250@gmail.com>
2023-03-20 12:42:15 -0700

    Print struck dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_dog - prints struct dog d
+ * @d: the struct dog variable
+ *
+ * Return: nothing (void)
+ */
+void print_dog(struct dog *d)
+{
+	char *dogname;
+	char *dogowner;
+
+	if (d == NULL)
+		return;
+	
+	if (d->name == NULL)
+		dogname = "Name: (nil)";
+	else
+		dogname = d->name;
+	
+	if (d->owner == NULL)
+		dogowner = "(nil)";
+	else
+		dogowner = d->owner;
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", dogname, d->age, dogowner);
+}
