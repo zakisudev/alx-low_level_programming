b02e54103efb72586acee70fefc794a1b9ea8806
zakisu250 <zakisu250@gmail.com>
2023-03-20 12:17:20 -0700

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,11 @@ typedef struct dog
 {
 	char *name;
 	float age;
-	char *ower;
-};
+	char *owner;
+}dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* ifndef DOG_H */
