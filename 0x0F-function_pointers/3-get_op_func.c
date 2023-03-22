481ea11f6d33463f9790bc365c0229d66322465d
zakisu250 <zakisu250@gmail.com>
2023-03-22 05:27:49 -0700

    Multiple calculation operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,15 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * get_op_func - pointer to a function. Function is determined by string s
+ * get_op_func - pointer to a function
  * @s: the string used to determine which function to use in return
  *
  * Return: int calculated through function that was chosen to be pointed to,
  * NULL if string s is not "+", "-", "*", "/", or "%"
  */
+
 int (*get_op_func(char *s))(int a, int b)
 {
 	op_t ops[] = {
@@ -22,13 +24,10 @@ int (*get_op_func(char *s))(int a, int b)
 
 	while (i < 5)
 	{
-		if (*s == *ops[i].op)
-			break;
-			i++;
+		if (strcmp(s, ops[i].op == 0))
+			return(ops[i].f);
+		
+		i++;
 	}
-	if (i == 5)
-	{
-		return (NULL);
-	}
-	return (ops[i].f);
+	return (0);
 }
