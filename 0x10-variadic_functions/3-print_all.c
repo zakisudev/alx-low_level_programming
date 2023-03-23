55be3de20f12eda4424595dac3a194e949fd1125
zakisu250 <zakisu250@gmail.com>
2023-03-23 14:24:05 -0700

    Print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,95 +2,51 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_char(va_list vargs);
-void print_int(va_list vargs);
-void print_float(va_list vargs);
-void print_str(va_list vargs);
-
 /**
- * print_all - prints according to type indicated by string format
- * @format: const string used to show type of upcoming variable arguments
- *
- * Return: nothing (void)
- */
+* print_all - prints anything
+* @format: list of types of arguments passed to the function
+*/
+
 void print_all(const char * const format, ...)
+
 {
 
-	char *choice_format[4] = {"c", "i", "f", "s"};
-	void (*choice_func[4])() = {print_char, print_int, print_float,
-		print_str};
-	int i = 0, j = 0;
-	va_list vargs;
-	char *separator = "";
+int i = 0;
+char *str, *sep = "";
 
-	va_start(vargs, format);
+va_list list;
 
-	while (format[i] != '\0' && format != NULL)
-	{
-		j = 0;
-		while (j < 4)
-		{
-			if (format[i] == choice_format[j][0])
-			{
-				printf("%s", separator);
-				choice_func[j](vargs);
-				separator = ", ";
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(vargs);
+va_start(list, format);
+
+if (format)
+{
+while (format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sep, va_arg(list, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(list, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(list, double));
+break;
+case 's':
+str = va_arg(list, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", sep, str);
+break;
+default:
+i++;
+continue;
 }
-
-/**
- * print_char - prints a char
- * @vargs: pointer to current position in va_list
- *
- * Return: nothing (void)
- */
-void print_char(va_list vargs)
-{
-	printf("%c", va_arg(vargs, int));
+sep = ", ";
+i++;
 }
-
-/**
- * print_int - prints a float
- * @vargs: pointer to current position in va_list
- *
- * Return: nothing (void)
- */
-void print_int(va_list vargs)
-{
-	printf("%d", va_arg(vargs, int));
 }
-
-/**
- * print_float - prints a float
- * @vargs: pointer to current position in va_list
- *
- * Return: nothing (void)
- */
-void print_float(va_list vargs)
-{
-	printf("%f", va_arg(vargs, double));
-}
-
-/**
- * print_str - prints a string
- * @vargs: pointer to current position in va_list
- *
- * Return: nothing (void)
- */
-void print_str(va_list vargs)
-{
-	char *str = va_arg(vargs, char *);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
+printf("\n");
+va_end(list);
 }
