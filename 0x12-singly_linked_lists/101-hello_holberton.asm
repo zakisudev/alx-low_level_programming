87342ff6be9c07082dd6bffc8122a5f156d676f5
zakisu250 <zakisu250@gmail.com>
2023-03-30 07:24:41 -0700

    Hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+	msg:	db "Hello, Holberton", 0
+	fmt:	db "%s", 10, 0
+
+
+section .text
+global main
+extern printf
+
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov ebx, 0
+	mov eax, 1
