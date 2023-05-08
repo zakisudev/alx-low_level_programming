13ee069eb854a98139123d584d25d9360f20b664
zakisu250 <zakisu250@gmail.com>
2023-05-08 04:35:35 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[len] != ‘\0’)
+		while (text_content[len] != '\0')
 			len++;
 		if (write(fd, text_content, len) == -1)
 		{
