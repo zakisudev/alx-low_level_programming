31b751796fffef794269928a97fa5dd42a792bce
zakisu250 <zakisu250@gmail.com>
2023-05-08 04:29:54 -0700

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -38,10 +38,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	output = write(STDOUT_FILENO, ch, output); 
+	output = write(STDOUT_FILENO, ch, output);
 	free(ch);
 	close(fd);
 	if (output == -1)
 		return (0);
-	return (output); 
+	return (output);
 }
