8e253b5a51968901da0402e6d2f8222e2d106aca
zakisu250 <zakisu250@gmail.com>
2023-06-15 03:20:13 -0700

    fixed betty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,7 +4,7 @@
  * add_dnodeint - Add new node at the beginning of a dlistint_t list
  * @head: struct
  * @n: node data
- * Return - address of the new element
+ * Return: address of the new element
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
@@ -19,5 +19,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head != NULL)
 		(*head)->prev = tmp;
 	*head = tmp;
-	return (tmp)
+	return (tmp);
 }
