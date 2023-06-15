8c58f71ceb90256ea5e15c53c11327f3114b7583
zakisu250 <zakisu250@gmail.com>
2023-06-15 03:18:26 -0700

    fixed betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,8 +2,8 @@
 
 /**
  * print_dlistint - Prints all elements of a dlistint_t list
- * @h: struct 
- * Return - number of nodes
+ * @h: struct
+ * Return: number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
