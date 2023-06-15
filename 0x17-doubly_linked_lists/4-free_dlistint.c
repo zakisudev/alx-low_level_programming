35c4396082fd51c9e127cb60da3cea700ce6a525
zakisu250 <zakisu250@gmail.com>
2023-06-15 02:22:45 -0700

    Free a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Free a dlistint_t list
+ * @head: struct node
+ * Return - void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *new = head;
+
+	while (new != NULL)
+	{
+		dlistint_t *next = new->next;
+		free(new);
+		new = next;
+	}
+}
