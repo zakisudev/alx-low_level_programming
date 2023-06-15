ff64ed00dc702bbb4a10d58d2ca009fd7f94eafa
zakisu250 <zakisu250@gmail.com>
2023-06-15 02:31:57 -0700

    Get the node at a position

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodelist_at_index - Return the nth node of a dlistint_t list
+ * @head: struct node
+ * @index: position of node
+ * Return - node or Null if it doesnt exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp = head;
+	unsigned int i = 0;
+
+	while (tmp != NULL && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp == NULL)
+		return (NULL);
+	else
+		return (tmp);
+}
