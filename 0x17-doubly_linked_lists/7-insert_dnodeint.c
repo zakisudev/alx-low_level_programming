1cc990dfeee133eb4e4decade28c0ce50e49bf4d
zakisu250 <zakisu250@gmail.com>
2023-06-15 03:49:32 -0700

    fixed betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,37 +1,13 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - Add new node at the beginning of a dlistint_t list
- * @head: struct
- * @n: node data
- * Return - address of the new element
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *tmp = mallod(sizeof(dlistint_t));
-
-	if (tmp == NULL)
-		return (NULL);
-	tmp->n = n;
-	tmp->next = *head;
-	tmp->prev = NULL;
-
-	if (*head != NULL)
-		(*head)->prev = tmp;
-	*head = tmp;
-	return (tmp)
-}
-
-#include "lists.h"
-
-/**
  * dlistint_t_len - Return the length of a dlistint_t list
  * @h: struct
  * Return: the length of the list
  */
 size_t dlistint_t_len(const dlistint_t *h)
 {
-	size_t count= 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
@@ -42,7 +18,7 @@ size_t dlistint_t_len(const dlistint_t *h)
 }
 
 /**
- * insert_dnodelist_at_index - Inserts a new node at given position
+ * insert_dnodeint_at_index - Inserts a new node at given position
  * @h: struct node
  * @idx: index of node to add
  * @n: node data
@@ -69,6 +45,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = curr;
 	new->next = curr->next;
 	curr->next->prev = new;
-	curr-next = new;
+	curr->next = new;
 	return (new);
 }
