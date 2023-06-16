ba02a424b4357962d5357e8acf39de027b129f55
zakisu250 <zakisu250@gmail.com>
2023-06-16 00:52:40 -0700

    fixed implementation

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,54 +1,47 @@
 #include "lists.h"
 
 /**
- * dlistint_t_len - Return the length of a dlistint_t list
- * @h: head for node
- * Return: the length of the list
- */
-size_t dlistint_t_len(const dlistint_t *h)
-{
-	size_t count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-/**
- * insert_dnodeint_at_index - Inserts a new node at given position
- * @h: struct node
- * @idx: index of node to add
- * @n: node data
- * Return: Address of new node or NULL if it failed
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ * @h: head
+ * @idx: index
+ * @n: data to be added
+ *
+ * Return: address of new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *curr;
-	unsigned int i;
+	dlistint_t *newN, *tmp = *h;
+	unsigned int i = 0;
 
-	if (idx > dlistint_t_len(*h))
-		return (NULL);
-
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->prev = NULL;
-	new->n = n;
-	new->next = NULL;
-
-	curr = *h;
-	for (i = 0; i < (idx - 1); i++)
+	while (tmp && i < idx - 1)
 	{
-		curr = curr->next;
+		tmp = tmp->next;
+		i++;
 	}
-	new->prev = curr;
-	new->next = curr->next;
-	curr->next->prev = new;
-	curr->next = new;
-	return (new);
+	if (tmp || (i == idx - 1 || idx == 0))
+	{
+		newN = malloc(sizeof(dlistint_t));
+		if (newN)
+		{
+			newN->n = n;
+			if (idx == 0)
+			{
+				newN->next = *h;
+				if (newN->next)
+					newN->next->prev = newN;
+				newN->prev = NULL;
+				*h = newN;
+			}
+			else
+			{
+				newN->next = tmp->next;
+				newN->prev = tmp;
+				tmp->next = newN;
+				if (newN->next)
+					newN->next->prev = newN;
+			}
+			return (newN);
+		}
+	}
+	return (NULL);
 }
