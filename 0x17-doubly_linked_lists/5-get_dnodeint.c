6904485986607ee602e4ff05c139998d930c5a12
zakisu250 <zakisu250@gmail.com>
2023-06-16 00:32:57 -0700

    return descrition added

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * get_dnodeint_at_index - Return the nth node of a dlistint_t list
  * @head: struct node
  * @index: position of node
- * Return - node or Null if it doesnt exist
+ * Return: node or Null if it doesnt exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
