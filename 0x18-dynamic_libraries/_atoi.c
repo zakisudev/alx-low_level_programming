e1d64d467591074131782746256ef1f5004053ee
zakisu250 <zakisu250@gmail.com>
2023-06-19 04:32:21 -0700

    While loop contained with curly bracket

--- 0x18-dynamic_libraries/_atoi.c
+++ 0x18-dynamic_libraries/_atoi.c
@@ -13,16 +13,16 @@ int _atoi(char *s)
 
 	while (*s == ' ')
 		s++;
-		if (*s == '-' || *s == '+')
-		{
-			if (*s == '-')
-				sign = -1;
-			s++;
-		}
-		while (*s >= '0' && *s <= '9')
-		{
-			result = (result * 10) + (*s - '0');
-			s++;
-		}
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		result = (result * 10) + (*s - '0');
+		s++;
+	}
 	return result * sign;
 }
