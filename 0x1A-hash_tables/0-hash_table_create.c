0ca98ca9ae10f097f0fdf066a18f008726701b98
zakisu250 <zakisu250@gmail.com>
2023-07-06 23:32:40 -0700

    Added array memory allocation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,20 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* new_ht = (hash_table_t*) malloc(sizeof(hash_table_t));
+	hash_table_t *new_ht = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
+
+	if (!new_ht)
+		return (NULL);
 
 	new_ht->size = size;
+	new_ht->array = malloc(sizeof(hash_table_t *) * size);
+
+	if (!new_ht->array)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		new_ht->array[i] = NULL;
+
 	return (new_ht);
 }
