22c7010f878a9da54366da2a19c24cf7c73414f2
zakisu250 <zakisu250@gmail.com>
2023-07-06 23:07:40 -0700

    Creating a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table with size
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t* new_ht = (has_table_t*) malloc(sizeof(hash_table_t));
+
+	new_ht->size = size;
+}
