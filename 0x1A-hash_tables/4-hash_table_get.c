751d307e00dd52b80c14c4ab882d5f629f8bd758
zakisu250 <zakisu250@gmail.com>
2023-07-07 00:34:07 -0700

    betty fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,16 +11,18 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int i = 0;
+	unsigned long int i;
 	hash_node_t *old;
 
 	if (!ht || !key || strlen(key) == 0)
 		return (NULL);
+
 	i = key_index((unsigned char *) key, ht->size);
 
 	if ((ht->array)[i] == NULL)
 		return (NULL);
-	old = (ht->array)[index];
+
+	old = (ht->array)[i];
 	while (old)
 	{
 		if (strcmp(old->key, key) == 0)
