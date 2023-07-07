330871ca8bfb1f0e26b2f2d2880203f1f724fe09
zakisu250 <zakisu250@gmail.com>
2023-07-06 23:11:33 -0700

    spelling fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,8 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* new_ht = (has_table_t*) malloc(sizeof(hash_table_t));
+	hash_table_t* new_ht = (hash_table_t*)malloc(sizeof(hash_table_t));
 
 	new_ht->size = size;
+	return (new_ht);
 }
